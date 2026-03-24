6f8125a01246a4bbd5db8a521eccd3f9109a99c6
khushalishah2611 <khushalishah2611@gmail.com>
2026-03-24 11:27:53 +0530

    Refactor shimmer into shared widget and separate home list skeleton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import '../core/app_localizations.dart';
 import '../core/app_theme.dart';
 import '../models/app_models.dart';
 import '../widgets/app_drawer.dart';
+import '../widgets/app_shimmer.dart';
 import '../widgets/common_widgets.dart';
 import '../widgets/flow_widgets.dart';
 import 'course_list_screen.dart';
@@ -155,7 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   ),
                               itemBuilder: (context, index) {
                                 if (_isLoading) {
-                                  return const _UniversityCardSkeleton();
+                                  return const UniversityCardSkeleton();
                                 }
                                 final item = universityCatalog[index];
                                 return _UniversityCard(
@@ -311,7 +312,7 @@ class _UniversityCard extends StatelessWidget {
                     fit: BoxFit.cover,
                     loadingBuilder: (context, child, loadingProgress) {
                       if (loadingProgress == null) return child;
-                      return _ShimmerBox(
+                      return AppShimmerBox(
                         borderRadius: BorderRadius.circular(10),
                         baseColor: const Color(0xFFE9E6E0),
                         highlightColor: Colors.white,
@@ -371,119 +372,6 @@ class _UniversityCard extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class _UniversityCardSkeleton extends StatelessWidget {
-  const _UniversityCardSkeleton();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: const Color(0xFFE8E3DB)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _ShimmerBox(
-                height: 96,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              const SizedBox(height: 10),
-              _ShimmerBox(
-                height: 14,
-                width: double.infinity,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              const SizedBox(height: 6),
-              _ShimmerBox(
-                height: 12,
-                width: 90,
-                borderRadius: BorderRadius.circular(4),
-              ),
-            ],
-          ),
-          _ShimmerBox(
-            height: 40,
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _ShimmerBox extends StatefulWidget {
-  const _ShimmerBox({
-    this.height,
-    this.width = double.infinity,
-    this.borderRadius,
-    this.baseColor = const Color(0xFFECE7DE),
-    this.highlightColor = const Color(0xFFF9F7F3),
-  });
-
-  final double? height;
-  final double width;
-  final BorderRadius? borderRadius;
-  final Color baseColor;
-  final Color highlightColor;
-
-  @override
-  State<_ShimmerBox> createState() => _ShimmerBoxState();
-}
-
-class _ShimmerBoxState extends State<_ShimmerBox>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1200),
-    )..repeat();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) {
-        final shimmerPosition = (_controller.value * 2) - 1;
-        return Container(
-          width: widget.width,
-          height: widget.height,
-          decoration: BoxDecoration(
-            borderRadius: widget.borderRadius ?? BorderRadius.circular(8),
-            gradient: LinearGradient(
-              begin: Alignment(-1.5 + shimmerPosition, -0.3),
-              end: Alignment(1.5 + shimmerPosition, 0.3),
-              colors: [
-                widget.baseColor,
-                widget.highlightColor,
-                widget.baseColor,
-              ],
-              stops: const [0.1, 0.5, 0.9],
-            ),
-          ),
-        );
-      },
     );
   }
 }
