a0402cacbdcf3fa4350f6c7aded3265f7c89b83b
khushalishah2611 <khushalishah2611@gmail.com>
2026-04-09 19:00:17 +0530

    course ui develop

--- lib/models/app_models.dart
+++ lib/models/app_models.dart
@@ -32,37 +32,6 @@ class CourseData {
   final String image;
 }
 
-const universityCatalog = [
-  UniversityData(
-    name: 'United Arab Emirates University',
-    location: 'United Arab Emirates',
-    shortCode: 'UAEU',
-    color: Color(0xFFE53935),
-    heroImage: 'https://images.unsplash.com/photo-1562774053-701939374585?auto=format&fit=crop&w=1200&q=80',
-  ),
-  UniversityData(
-    name: 'Al-Ahliyya Amman University',
-    location: 'Jordan',
-    shortCode: 'AAU',
-    color: Color(0xFF486AAE),
-    heroImage: 'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?auto=format&fit=crop&w=1200&q=80',
-  ),
-  UniversityData(
-    name: 'Beirut Arab University',
-    location: 'Lebanon',
-    shortCode: 'BAU',
-    color: Color(0xFF7092B9),
-    heroImage: 'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?auto=format&fit=crop&w=1200&q=80',
-  ),
-  UniversityData(
-    name: 'University of Sharjah',
-    location: 'Sharjah',
-    shortCode: 'UOS',
-    color: Color(0xFF3A9D58),
-    heroImage: 'https://images.unsplash.com/photo-1564981797816-1043664bf78d?auto=format&fit=crop&w=1200&q=80',
-  ),
-];
-
 const courseCatalog = [
   CourseData(
     title: 'Bachelor of Computer Science',
