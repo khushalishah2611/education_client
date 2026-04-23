6191be6d25aa6c2e1f9b5e5d9b0dcde9480b6c99
khushalishah2611 <khushalishah2611@gmail.com>
2026-04-23 13:36:22 +0530

    course ui develop

--- lib/core/api_config.dart
+++ lib/core/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://arab.vedx.cloud:8000';
+  static const String baseUrl = 'https://arab.vedx.cloud';
 
   static Uri uri(String path) => Uri.parse('$baseUrl$path');
 }
