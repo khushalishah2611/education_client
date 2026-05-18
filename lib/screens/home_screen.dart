5e77272e01361c7fde57683211a45ffe35099580
vedx solutions <khushalishah2611@gmail.com>
2026-05-18 17:28:10 +0530

    Wotking on

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -511,7 +511,7 @@ class _DiscoverBanner extends StatelessWidget {
       return const _BannerShimmer();
     }
     return Container(
-      height: 180,
+      height: 160,
       width: double.infinity,
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(14)),
       child: ClipRRect(
@@ -565,7 +565,7 @@ class _BannerShimmer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120,
+      height: 160,
       padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         color: Colors.white,
@@ -574,7 +574,7 @@ class _BannerShimmer extends StatelessWidget {
       ),
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [_ShimmerBlock(height: 100, borderRadius: 8)],
+        children: [_ShimmerBlock(height: 160, borderRadius: 8)],
       ),
     );
   }
