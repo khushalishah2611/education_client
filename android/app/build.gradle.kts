cdc28bee6ac78e6d3558bab8f41e58252f2d5ff6
khushalishah <khushalishah2611@gmail.com>
2026-06-25 16:12:06 +0530

    package name is change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.arab.education"
+    namespace = "com.arab.university"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.arab.education"
+        applicationId = "com.arab.university"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/arab/education/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.arab.education
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
